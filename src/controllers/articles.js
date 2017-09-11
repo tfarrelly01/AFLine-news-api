6ba3cd17ff778af89a548c942363d3e9ec8242a2
Tony Farrelly <tfarrelly01@gmail.com>
2017-09-11 17:35:31 +0100

    GET /articles - returns all articles including a comment count for each article - Route IMPLEMENTED

--- src/controllers/articles.js
+++ src/controllers/articles.js
@@ -37,11 +37,34 @@ exports.getAllArticlesByTopic = (req, res, next) => {
 };
 
 exports.getAllArticles = (req, res, next) => {
-    Articles.find()
-        .then((articles) => {
-            if (articles.length < 1) return next({ status: 404, message: 'No Articles Found' });
+  // for each article, read the comments collection and count number of comments 
+  // made against the article
+  Articles.find({})
+    .then((articles) => {
+      if (articles.length < 1) return next({ status: 404, message: 'No Articles Found' });
 
-            return res.status(200).json({ articles });
+      const commentCounts = articles.map((article) => {
+        // count number of comments made against each article found
+        return Comments.count({belongs_to: article._id});
+      });
+
+      Promise.all([articles, ...commentCounts])
+        .then(([articles, ...commentCounts]) => {
+          const articlesWithCommentCount = articles.map((article, i) => {
+            return {
+              _id: article._id,
+              title: article.title,
+              body: article.body,
+              created_by: article.created_by,
+              belongs_to: article.belongs_to,
+              votes: article.votes,
+              __v: article.__v,
+              comments: commentCounts[i]
+            };
+          });
+          return res.status(200).json({articles: articlesWithCommentCount});
         })
         .catch(next);
+    })
+    .catch(next);
 };
