de1781dc273338b4dc55afc5ef5ee41f549d8ba9
Tony Farrelly <tfarrelly01@gmail.com>
2017-09-11 13:51:52 +0100

    Configuration file for Development and Test databases created

---
+++ src/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  DB: {
+    test: 'mongodb://localhost/af-line-news-api-test',
+    dev: 'mongodb://localhost/af-line-news-api'
+  },
+  PORT: {
+    test: 3091,
+    dev: 3000
+  }
+};
