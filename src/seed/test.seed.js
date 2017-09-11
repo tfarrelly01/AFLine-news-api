43301df1a7e0475f821e8893ecff826af72dbf10
Tony Farrelly <tfarrelly01@gmail.com>
2017-09-11 14:31:56 +0100

    Test Database: topics model created and populated with data

--- src/seed/test.seed.js
+++ src/seed/test.seed.js
@@ -9,6 +9,12 @@ const user = new models.Users({
   avatar_url: 'https://www.linkedin.com/in/tfarrelly01'
 });
 
+const topics = [
+  new models.Topics({ title: 'Football', slug: 'football' }),
+  new models.Topics({ title: 'Cooking', slug: 'cooking' }),
+  new models.Topics({ title: 'Cats', slug: 'cats' })
+];
+
 function saveUser(cb) {
   user.save((err) => {
     if (err) cb(err);
@@ -16,8 +22,15 @@ function saveUser(cb) {
   });
 }
 
+function saveTopics(cb) {
+  models.Topics.create(topics, (err) => {
+    if (err) cb(err);
+    else cb();
+  });
+}
+
 function saveTestData(DB, cb) {
-    async.waterfall([saveUser], (err, ids) => {
+    async.waterfall([saveUser, saveTopics], (err, ids) => {
       if (err) cb(err);
       else {
         console.log('Test data seeded successfully.');
