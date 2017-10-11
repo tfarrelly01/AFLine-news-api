93d3f5c5f516d608c24d520967b4909b84e26970
Tony Farrelly <tfarrelly01@gmail.com>
2017-10-11 22:52:36 +0100

    Modify seed data

--- src/seed/data/user_data.js
+++ src/seed/data/user_data.js
@@ -1,8 +1,8 @@
 module.exports = [
   {username: 'tickle122', name: 'Tom Tickle', avatar_url: 'http://www.spiritsurfers.net/monastery/wp-content/uploads/_41500270_mrtickle.jpg'},
   {username: 'grumpy19', name: 'Paul Grump', avatar_url: 'http://www.tumbit.com/profile-image/4/original/mr-grumpy.jpg'},
-  {username: 'happyamy2016', name: 'Amy Happy', avatar_url: 'http://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729'},
-  {username: 'cooljmessy', name: 'Peter Messy', avatar_url: 'http://i.imgur.com/WfX0Neu.jpg'},
-  {username: 'weegembump', name: 'Gemma Bump', avatar_url: 'http://www.upandrunning.co.uk/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/m/r/mr-bump.jpg'},
+  {username: 'happyamy2016', name: 'Amy Happy', avatar_url: 'https://www.awn.com/sites/default/files/styles/inline/public/image/attached/3631-strike02mrmenshow-littlemi.jpg?itok=lgc4Nl_z'},
+  {username: 'cooljmessy', name: 'Peter Messy', avatar_url: 'https://i.pinimg.com/736x/3d/89/e3/3d89e38897edeb18ae5bb9a45036349c--mr-men-little-miss-mr-perfect.jpg'},
+  {username: 'weegembump', name: 'Gemma Bump', avatar_url: 'https://i.pinimg.com/736x/00/78/37/007837783dddc5111b5ef5c165ee5412--little-miss-characters-mr-men-books.jpg'},
   {username: 'jessjelly', name: 'Jess Jelly', avatar_url: 'https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg'}
 ];
