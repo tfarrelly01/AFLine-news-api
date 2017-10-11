f74aa4c8a9937c9814ca789dc3802fc1fff97387
Tony Farrelly <tfarrelly01@gmail.com>
2017-10-11 22:54:14 +0100

    Remove start.js - no longer required

--- start.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-console */
-const app = require('./src/server');
-const { PORT } = process.env;
-
-app.listen(PORT, function () {
-  console.log(`listening on port ${PORT}`);
-});
