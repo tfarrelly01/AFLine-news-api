888e4a9c62e1dcb1d7522182c0b364483cf7cdab
Tony Farrelly <tfarrelly01@gmail.com>
2017-10-17 09:36:41 +0100

    Refactor router (index.js) remove unecessary comments

--- src/router/index.js
+++ src/router/index.js
@@ -17,45 +17,29 @@ const { getAllCommentsByArticle,
 
 const { getAllUsers, getUser } = require('../controllers/users');
 
-// | `GET /api/topics` | Get all the topics |
 router.get('/topics', getAllTopics);
 
-// | `GET /api/topics/:topic_id/articles` | Return all the articles for a particular topic |
 router.get('/topics/:topic_id/articles', getAllArticlesByTopic);
 
-// | `GET /api/articles` | Get all the articles |
 router.get('/articles', getAllArticles);
 
-// | `GET /api/articles/:article_id` | Get individual article by id |
-// | `PUT /api/articles/:article_id` | Increment or Decrement the votes of an article by one. This route
-// requires a vote query of 'up' or 'down' i.e. /api/articles/:article_id?vote=up OR /api/articles/:article_id?vote=down |
 router.route('/articles/:article_id')
     .get(getArticleById)
     .put(updateArticleVote);
 
-// | `GET /api/articles/:article_id/comments` | Get all comments for an individual article |
-// | `POST /api/articles/:article_id/comments` | Add a new comment to an article. This route requires a 
-// JSON body with  a comment key and value pair e.g: {"comment": "This is my new comment"} |
 router.route('/articles/:article_id/comments')
     .get(getAllCommentsByArticle)
     .post(addNewComment);
 
-// |`GET /api/comments` | Get all the comments |
 router.get('/comments', getAllComments);
 
-// | `GET /api/comments/:comment_id` | Get comment by id |
-// | `PUT /api/comments/:comment_id` | Increment or Decrement the votes of a comment by one. This route
-// requires a vote query of 'up' or 'down' i.e. /api/comments/:comment_id?vote=up OR /api/comments/:comment_id?vote=down |
-// | `DELETE /api/comments/:comment_id` | Deletes a comment |
 router.route('/comments/:comment_id')
     .get(getCommentById)
     .put(updateCommentVote)
     .delete(deleteCommentById);
 
-// | `GET /api/users` | Returns a JSON object with all user profile data. |
 router.get('/users', getAllUsers);
 
-// | `GET /api/users/:username` | Returns a JSON object with the profile data for the specified user. |
 router.get('/users/:username', getUser);
 
 module.exports = router;
